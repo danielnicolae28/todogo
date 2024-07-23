20a2fd9a4073076b803d92e13488eed11a23b93e
danielnicolae28 <danielnicolae286@gmail.com>
2024-07-23 22:19:33 +0300

    done v1

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("todo")
+	var inputToDo string
+	todos := []string{}
+	// todos := make([]string, 100)
+	fmt.Println("To Do List")
+	for {
+		fmt.Print("Write To Do: ")
+		fmt.Scan(&inputToDo)
+		length := len(inputToDo)
+		if length < 3 {
+			fmt.Println("Input is to short")
+		}
+		todos = append(todos, inputToDo)
+
+		fmt.Println(todos)
+	}
+
 }
